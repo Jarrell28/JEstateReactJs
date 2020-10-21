337af5886ce3e707cb46295ff8c1ee4c25254f49
redimimi <redimimi@gmail.com>
2020-10-20 21:17:35 -0500

    work on home page

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import '../css/app.css';
 
 import Home from "./Home";
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import "../css/home.css";
+import vid from "../img/background-video1.mp4";
 
 class Home extends React.Component {
     render() {
         return (
             <div>
                 <div id="background">
+                    <div class="overlay"></div>
+                    <video id="video" autoPlay muted loop>
+                        <source src={vid} type="video/mp4" />
+                            Your browser does not support the video tag.
+                    </video>
+
                     <div className="content-container">
                         <h1>Find Your Home</h1>
 
